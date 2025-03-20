52b04e6e13187737fa591ef620962ee37a8af9c2
Marco <marco.palummieri@gmail.com>
2025-03-20 09:23:41 +0100

    add new div container

---
+++ scripts/db.js
@@ -0,0 +1,27 @@
+let dishes = [
+    {
+      "name": "Burger",
+      "sidedish": "Pommes und Salat",
+      "price": 5.90,
+    },
+    {
+        "name": "Burger Berger",
+        "sidedish": "Pommes und Salat",
+        "price": 7.90,
+      },
+      {
+        "name": "Burger",
+        "sidedish": "Pommes und Salat",
+        "price": 8.90,
+      },
+      {
+        "name": "Burger",
+        "sidedish": "Pommes und Salat",
+        "price": 9.90,
+      },
+      {
+        "name": "Burger",
+        "sidedish": "Pommes und Salat",
+        "price": 12.90,
+      },
+]
