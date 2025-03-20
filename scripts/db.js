2c26c48ca185abd2d02a0affbb941ff5a8478fc5
Marco <marco.palummieri@gmail.com>
2025-03-20 13:52:10 +0100

    update 03

--- scripts/db.js
+++ scripts/db.js
@@ -24,4 +24,25 @@ let dishes = [
         "sidedish": "Pommes und Salat",
         "price": 12.90,
       },
+      {
+        "name": "Burger",
+        "sidedish": "Pommes und Salat",
+        "price": 5.90,
+      },
+    ]
+    
+let sideDishes [
+      {
+          "name": "Pommes",
+          "price": 7.90,
+        },
+        {
+          "name": "Süßkartoffel-Pommes",
+          "price": 8.90,
+        },
+        {
+          "name": "Pommes Spezial",
+          "sidedish": "Zwiebeln",
+          "price": 9.90,
+        },
 ]
