42cae217e19f5ad04aaffc1b3694cb8bdf388b6b
Marco <marco.palummieri@gmail.com>
2025-03-21 13:41:57 +0100

    update functions

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 function init(){
     renderMainDishes()
     renderSideDishes()
+    
 }
 
 // Gerichte rendern
 function renderMainDishes(){
     
-    for (let i = 0; i < dishes.length; i++) {
-        document.getElementById('render_dishes').innerHTML += getDishes(i);
+    for (let dishesIndex = 0; dishesIndex < dishes.length; dishesIndex++) {
+        document.getElementById('render_dishes').innerHTML += getDishes(dishesIndex);
     } 
 }
 
 function renderSideDishes(){
 
-  for (let i = 0; i < sideDishes.length; i++) {
-    document.getElementById('render_side_dishes').innerHTML += getSideDishes(i);
+  for (let sideDishesindex = 0; sideDishesindex < sideDishes.length; sideDishesindex++) {
+    document.getElementById('render_side_dishes').innerHTML += getSideDishes(sideDishesindex);
   }
 }
 
 
 
 // Menü dem Warenkorb hinzufügen
-function addMenu(){}
+function addMenu(dishesIndex){
+  document.getElementById('render_basket_order').innerHTML += getBasketOrder(dishesIndex);
+  dishes[dishesIndex].amount += 1
+  console.log(dishes[dishesIndex].amount);
+  
+}
 
 
--- scripts/template.js
+++ scripts/template.js
@@ -1,10 +1,10 @@
-function getDishes(i){
+function getDishes(dishesIndex){
     return `
-        <div id="dishes_box" onclick="addMenu()">
+        <div id="dishes_box" onclick="addMenu(${[dishesIndex]})">
             <div>
-              <p class="dishes_box_headline">${dishes[i].name}</p>
-              <p class="dishes_box_menu">${dishes[i].info}</p>
-              <p class="dishes_box_price">${dishes[i].price.toFixed(2)} €</p>
+              <p class="dishes_box_headline">${dishes[dishesIndex].name}</p>
+              <p class="dishes_box_menu">${dishes[dishesIndex].info}</p>
+              <p class="dishes_box_price">${dishes[dishesIndex].price.toFixed(2)} €</p>
             </div>
             <div class="add_btn">
               <img class="basket_icon" src="./Assets/icons/icons8-warenkorb.png" alt="">
@@ -13,15 +13,36 @@ function getDishes(i){
     `;
 }
 
-function getSideDishes(i){
+function getSideDishes(sideDishesindex){
 return `<div id="dishes_box" onclick="addMenu()">
             <div>
-              <p class="dishes_box_headline">${sideDishes[i].name}</p>
-              <p class="dishes_box_price">${sideDishes[i].price.toFixed(2)} €</p>
+              <p class="dishes_box_headline">${sideDishes[sideDishesindex].name}</p>
+              <p class="dishes_box_price">${sideDishes[sideDishesindex].price.toFixed(2)} €</p>
             </div>
             <div class="add_btn">
               <img class="basket_icon" src="./Assets/icons/icons8-warenkorb.png" alt="">
             </div>
           </div>
+    `;
+}
+
+
+function getBasketOrder(dishesIndex){
+    return `<div class="basket_order">
+              <p class="dishes_box_headline">${dishes[dishesIndex].name}</p>
+              <div class="order_options">
+                <button class="plus_btn">
+                  <img src="./Assets/icons/icons8-plus-weiß.png" alt="">
+                </button>
+                <p>${dishes[dishesIndex].amount+1}</p>
+                <button class="minus_btn">
+                  <img src="./Assets/icons/icons8-minus-weiß.png" alt="">
+                </button>
+                <p>${dishes[dishesIndex].price.toFixed(2)} €</p>
+                <button class="delete_btn">
+                  <img src="./Assets/icons/icons8-müll-white.png" alt="">
+                </button>
+              </div>
+            </div>
     `;
 }
